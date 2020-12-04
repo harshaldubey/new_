2e3a3f5ce5ad3e478f8d02cd9c7435e5623f44c9
Vighnesh Kulkarni <vighnesh192000@gmail.com>
2020-12-04 14:15:50 +0530

    passing query params for dynamic category input field

--- band.js
+++ band.js
@@ -1,6 +1,11 @@
+var id = '';
 
-          function myFunction(id) {
-          console.log(id);
-          document.getElementById("exampleInputEmail1").value = id;
-          console.log(document.getElementById("exampleInputEmail1"));
-          }
+function myFunction(id) {
+    var value1="value1";
+    var value2="value2";
+    var queryString = id;
+    window.location.href = "form.html" + '?' + queryString;
+    console.log('Id', id);
+    console.log(document.getElementById("exampleInputEmail1"));
+    document.getElementById("exampleInputEmail1").value = 'id';
+}
