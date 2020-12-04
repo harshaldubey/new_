4f69cb252f1e503d86443393173705ae012b54f9
Harshal Dubey <68217194+harshaldubey@users.noreply.github.com>
2020-12-04 14:46:24 +0530

    message

---
+++ band.js
@@ -0,0 +1,6 @@
+
+          function myFunction(id) {
+          console.log(id);
+          document.getElementById("exampleInputEmail1").value = id;
+          console.log(document.getElementById("exampleInputEmail1"));
+          }
